de8b1a07bcc6ac7d476d06eca96a88f9e533fe1c
rlu789 <rlu789@hotmail.com>
2018-09-25 19:26:35 +1000

    alert css

--- Dotnet_Angular/ClientApp/src/app/Custom Components/custom-alert/custom-alert.component.ts
+++ Dotnet_Angular/ClientApp/src/app/Custom Components/custom-alert/custom-alert.component.ts
@@ -9,20 +9,34 @@ export class CustomAlertComponent implements OnInit {
   @Input('title') title: string;
   @Input('content') content: string;
   @Input('type') type: string;
-  class = 'icon ';
+  @Input('isVisible') isVisible: boolean;
+  @Input('dismissible') dismissible: boolean;
+  typeClass: string;
+  iconClass = 'icon ';
 
   constructor() {
   }
 
   ngOnInit() {
+    this.isVisible = this.isVisible !== undefined ? this.isVisible: true;
     switch (this.type) {
       case 'i':
-        this.class += 'fas fa-info-circle fa-2x';
+        this.typeClass = 'info';
+        this.iconClass += 'fas fa-info-circle fa-2x';
         break;
-    
+      case 'w':
+        this.typeClass = 'warning';
+        this.iconClass += 'fas fa-exclamation-circle fa-2x';
+        break;
+      case 's':
+        this.typeClass = 'success';
+        this.iconClass += 'fas fa-check-circle fa-2x';
       default:
         break;
     }
   }
 
+  dismiss(){
+      this.typeClass += ' dismissed';
+  }
 }
